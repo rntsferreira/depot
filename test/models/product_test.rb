29763a07a5d453b58fe46182b1f86d0647035a0c
Renato Ferreira <renato.ferreira@mobihouse.com>
2014-06-16 15:34:53 -0300

    Validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	fixtures :products
+	test "product is not valid without a unique title"
+		product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+		assert product.invalid?
+		assert_equal ["has already been taken"], product.errors[:title]
+	end
+	test "product attributes must not be empty" do
+		product = Product.new(title: "My book title", description: "yyy", image_url: "zzz.jpg")
+
+		product.price = -1
+
+		assert product.invalid?
+		assert_equal ["must be greater than or equal to 0.01"],
+			product.errors[:price]
+
+		product.price = 1
+		assert product.valid?
+	end
+	def new_product(image_url)
+		Product.new(title: "My book title", description: "yyy", price: 1,  image_url: image_url)
+	end
+	test "image url" do
+		ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+		bad = %w{ fred.doc fred.gif/more fred.gif.more }
+		ok.each do |name|
+			assert new_product(name).valid?, "#{name} should be valid"
+		end
+		bad.each do |name|
+			assert new_product(name).invalid?, "#{name} shouldn't be valid"
+		end
+	end
 end
