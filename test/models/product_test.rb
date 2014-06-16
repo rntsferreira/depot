e320b74f226f449975e10825389d8d8ff3f034a7
Renato Ferreira <renato.ferreira@mobihouse.com>
2014-06-16 15:42:04 -0300

    Validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
 	fixtures :products
-	test "product is not valid without a unique title"
-		product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+	test "product is not valid without a unique title" do
+		product = Product.new(	title: products(:ruby).title, 
+								description: "yyy", 
+								price: 1, 
+								image_url: "fred.gif")
 		assert product.invalid?
 		assert_equal ["has already been taken"], product.errors[:title]
 	end
