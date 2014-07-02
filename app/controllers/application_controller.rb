fa08bb8d1d29accd169867122c5bd33c8015a5e8
Renato Ferreira <renato.ferreira@mobihouse.com>
2014-07-02 19:03:58 -0300

    delete user: verify if is last user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,5 @@ class ApplicationController < ActionController::Base
         redirect_to login_url, notice: "Please log in"
       end
     end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   # It validates that the two passwords match
   has_secure_password
-end
+
+  after_destroy :ensure_an_admin_remains
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end
+  end
